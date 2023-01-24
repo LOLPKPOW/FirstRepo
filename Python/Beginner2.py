a380c4ad450c82bc5d96d18c5486236d7799d520
LOLPKPOW <patrick.wooddward8@gmail.com>
2023-01-24 09:03:00 -0600

    Practice makes Perfect

--- Python/Beginner2.py
+++ Python/Beginner2.py
@@ -1,6 +1,9 @@
 prompt = "Enter a value:"
 count = 1
+user_inputs = []
 
 while count < 4:
     user_input = input(prompt)
+    user_inputs.append(user_input.capitalize())
     count = count + 1
+    print(user_inputs)
