4290c692da438e5bd7650dab7f5030deb3d7078f
LOLPKPOW <patrick.wooddward8@gmail.com>
2023-01-24 09:13:31 -0600

    Commit

--- Python/Beginner2.py
+++ Python/Beginner2.py
@@ -4,6 +4,6 @@ user_inputs = []
 
 while count < 4:
     user_input = input(prompt)
-    user_inputs.append(user_input.capitalize())
+    user_inputs.append(user_input.title())
     count = count + 1
     print(user_inputs)
