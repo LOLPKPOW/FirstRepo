e8b74e89dbf3f10e344cfd2ad2974d0ab9a8eaa0
Patrick Woodward <pwoodward@pcatechsolutions.com>
2023-01-30 16:58:37 -0600

    Added generic list for testing,

--- Python/Beginner3.py
+++ Python/Beginner3.py
@@ -2,6 +2,8 @@ user_prompt = "Type add, show, edit or exit: "
 user_list = []
 x =1 
 
+# user_list = [List1, List2, List3, List5]
+
 while True:
     user_action = input(user_prompt)
     user_action = user_action.strip()
